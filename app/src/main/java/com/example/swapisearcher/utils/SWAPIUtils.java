53e6358f7cef71fd2b8738d2f628cb366cfafc6d
Skinner <Nick.Skinner@dnvgl.com>
2019-03-01 09:22:45 -0800

    Changed Color, added box, added Wookiee support
    wookie language is now implemented

--- app/src/main/java/com/example/swapisearcher/utils/SWAPIUtils.java
+++ app/src/main/java/com/example/swapisearcher/utils/SWAPIUtils.java
@@ -5,6 +5,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
+
+import com.example.swapisearcher.R;
+
 import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -18,6 +23,7 @@ public class SWAPIUtils {
     private final static String SWAPI_People = "people";
     private final static String SWAPI_WOOKIE_FORMAT = "format";
     private final static String SWAPI_WOOKIE_WOOKIE = "wookiee";
+    private final static Boolean wookiepref= true;
 
     public static class SWAPIItem implements Serializable{
         public static final String EXTRA_SWAPI_ITEM = "com.example.swapisearcher.utils.SWAPIItem.SearchResult";
@@ -33,33 +39,56 @@ public class SWAPIUtils {
         //public String name;
     }
 
-    public static String buildSWAPIURL(){
-        return Uri.parse(SWAPI_Base_URL).buildUpon()
-                .appendPath(SWAPI_People)
-                .toString();
+    public static String buildSWAPIURL(String lang){
+
+
+        if (lang.equals("Wookiee")) {
+            return Uri.parse(SWAPI_Base_URL).buildUpon()
+                    .appendPath(SWAPI_People)
+                    .appendQueryParameter(SWAPI_WOOKIE_FORMAT, SWAPI_WOOKIE_WOOKIE)
+                    .toString();
+        }else{
+            return Uri.parse(SWAPI_Base_URL).buildUpon()
+                    .appendPath(SWAPI_People)
+                    .toString();
+        }
     }
 
-    public static ArrayList<SWAPIItem> parseSWAPIJSON(String SWAPIJSON){
+    public static ArrayList<SWAPIItem> parseSWAPIJSON(String SWAPIJSON, String lang){
         try{
             JSONObject SWAPIOBJ = new JSONObject(SWAPIJSON);
-            JSONArray SWAPIList =  SWAPIOBJ.getJSONArray("results");
+            JSONArray SWAPIList;
+            if(lang.equals("Wookiee")) {
+                SWAPIList = SWAPIOBJ.getJSONArray("rcwochuanaoc");
+            }else{
+                SWAPIList = SWAPIOBJ.getJSONArray("results");
+            }
 
             ArrayList<SWAPIItem> SWAPIItemsList = new ArrayList<SWAPIItem>();
             for ( int i = 0; i< SWAPIList.length(); i++){
                 SWAPIItem swapiItem = new SWAPIItem();
 
                 JSONObject SWAPIListElem = SWAPIList.getJSONObject(i);
-
-                //String CharName = SWAPIListElem.getString("name");
-                swapiItem.name = SWAPIListElem.getString("name");
-                swapiItem.height = SWAPIListElem.getInt("height");
-                swapiItem.mass = SWAPIListElem.getString("mass");
-                swapiItem.hair_color = SWAPIListElem.getString("hair_color");
-                swapiItem.skin_color = SWAPIListElem.getString("skin_color");
-                swapiItem.eye_color = SWAPIListElem.getString("eye_color");
-                swapiItem.birth_year = SWAPIListElem.getString("birth_year");
-                swapiItem.gender = SWAPIListElem.getString("gender");
-
+                if (lang.equals("Wookiee")) {
+                    //String CharName = SWAPIListElem.getString("name");
+                    swapiItem.name = SWAPIListElem.getString("whrascwo");
+                    swapiItem.height = SWAPIListElem.getInt("acwoahrracao");
+                    swapiItem.mass = SWAPIListElem.getString("scracc");
+                    swapiItem.hair_color = SWAPIListElem.getString("acraahrc_oaooanoorc");
+                    swapiItem.skin_color = SWAPIListElem.getString("corahwh_oaooanoorc");
+                    swapiItem.eye_color = SWAPIListElem.getString("worowo_oaooanoorc");
+                    swapiItem.birth_year = SWAPIListElem.getString("rhahrcaoac_roworarc");
+                    swapiItem.gender = SWAPIListElem.getString("rrwowhwaworc");
+                }else{
+                    swapiItem.name = SWAPIListElem.getString("name");
+                    swapiItem.height = SWAPIListElem.getInt("height");
+                    swapiItem.mass = SWAPIListElem.getString("mass");
+                    swapiItem.hair_color = SWAPIListElem.getString("hair_color");
+                    swapiItem.skin_color = SWAPIListElem.getString("skin_color");
+                    swapiItem.eye_color = SWAPIListElem.getString("eye_color");
+                    swapiItem.birth_year = SWAPIListElem.getString("birth_year");
+                    swapiItem.gender = SWAPIListElem.getString("gender");
+                }
                 SWAPIItemsList.add(swapiItem);
             }
             return SWAPIItemsList;
